d91f5603604a9466d9f52fa0f5cfaf80a0b69567
Matthew <matthewjames.ns@googlemail.com>
2017-11-06 19:37:44 -0400

    Load demo implementation

--- app/src/main/java/mc/group/seven/mcprojectlightshift/LoadGameState.java
+++ app/src/main/java/mc/group/seven/mcprojectlightshift/LoadGameState.java
@@ -1,6 +1,7 @@
 package mc.group.seven.mcprojectlightshift;
 
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -14,6 +15,9 @@ public class LoadGameState extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loadgameinfo);
+
+        Intent intent = new Intent(this, SaveActivity.class);
+        startActivity(intent);
 
         t1 = (TextView) findViewById(R.id.loadname);
         t2 = (TextView) findViewById(R.id.loadscore);
