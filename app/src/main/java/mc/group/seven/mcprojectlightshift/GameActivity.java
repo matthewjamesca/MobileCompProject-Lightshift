a978562ce01af5e17d2047656a2dfacfb0f93e54
xliu <xn801678@dal.ca>
2017-11-06 21:48:30 -0400

    animation

--- app/src/main/java/mc/group/seven/mcprojectlightshift/GameActivity.java
+++ app/src/main/java/mc/group/seven/mcprojectlightshift/GameActivity.java
@@ -15,10 +15,17 @@ public class GameActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        overridePendingTransition(R.anim.pull_in_from_left, R.anim.hold);
         setContentView(R.layout.activity_game);
 
         gv = (GridView) this.findViewById(R.id.mygrid);
         CustomGridAdapter gridAdapter = new CustomGridAdapter(GameActivity.this, items);
         gv.setAdapter(gridAdapter);
+    }
+
+    @Override
+    protected void onPause() {
+        overridePendingTransition(R.anim.hold, R.anim.pull_out_to_left);
+        super.onPause();
     }
 }
