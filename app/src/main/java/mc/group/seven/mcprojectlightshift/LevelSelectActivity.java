8562a854b0d21507fe70ac91118bb2b67afb5294
kietle <kietle@users.noreply.github.com>
2017-11-19 14:10:11 -0400

    Level Selected

--- app/src/main/java/mc/group/seven/mcprojectlightshift/LevelSelectActivity.java
+++ app/src/main/java/mc/group/seven/mcprojectlightshift/LevelSelectActivity.java
@@ -9,5 +9,16 @@ public class LevelSelectActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_level_select);
+
+        /* --this will be the load function to load play stats--
+
+            SharedPreferences shared = getSharedPreferences("shareInfor", Context.MODE_PRIVATE);
+
+            SharedPreferences.Editor editor = shared.edit();
+            editor.putString("username", latitude);
+            editor.putString("score", longitude);
+            editor.apply();
+
+*/
     }
 }
